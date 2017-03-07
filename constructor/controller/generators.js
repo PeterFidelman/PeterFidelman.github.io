fcf87fc72e267a344af78968f1018b6ab6a3a53d
Peter Fidelman <edlinfan@gmail.com>
2017-03-06 21:46:43 -0800

    replaced copy-pasta'd constructor code with a proper git submodule

--- constructor/controller/generators.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-// TODO:  Will eventually contain generators to automate the work of creating
-// models.  See:
-// https://github.com/DannickFox/phyzzy-js/blob/master/js/builders.js
--- constructor/ui/widgets/dropdown.js
+++
@@ -1,3 +0,0 @@
-// Unused, but may be implemented some day for better look-and-feel.
-// Currently all "dropdowns" are implemented as carousel-style buttons instead
-// which round-robin through the possible choices when clicked.
